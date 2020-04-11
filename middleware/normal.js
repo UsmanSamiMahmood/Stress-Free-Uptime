2651eaeb569120510798c29e90ea5c988c751104
SergeantShadoww <47951918+SergeantShadoww@users.noreply.github.com>
2020-04-12 00:26:27 +0100

    Update normal.js

--- middleware/normal.js
+++ middleware/normal.js
@@ -4,17 +4,20 @@ const { db } = require("../database/handler");
 let location = db.collection("data").doc("permissionCheck")
     .get().then((doc) => {
         let blackListedIPs = doc.data().blacklistedIPs
-    
-    console.log(blackListedIPs);
-    
 
 router.get("/", (req, res, next) => {
-    return res.render("index");
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    ip = ip.split("::ffff:")//[0]
+    console.log(ip)
+    if (blackListedIPs.includes(ip)) {
+        return res.write(`Your IP ${ip} is blacklisted from using our services, have a good day.`)
+    } else {
+        return res.render("index");
+    }
 })
-
+});
 router.get("/login", (req, res, next) => {
     return res.render("login")
 })
-});
 
 module.exports = router;
