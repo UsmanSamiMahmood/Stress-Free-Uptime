5384074490c46d6c516d1abc16a9d93300bbcae8
SergeantShadoww <47951918+SergeantShadoww@users.noreply.github.com>
2020-04-12 00:42:26 +0100

    Added ip blacklisting check.

--- middleware/normal.js
+++ middleware/normal.js
@@ -6,18 +6,27 @@ let location = db.collection("data").doc("permissionCheck")
         let blackListedIPs = doc.data().blacklistedIPs
 
 router.get("/", (req, res, next) => {
-    let hey = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    hey = hey.split("::ffff:")[1]
-    console.log(hey)
-    if (blackListedIPs.includes(hey)) {
-        res.send(`Your IP ${hey} is blacklisted from using our services, have a good day.`)
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    ip = ip.split("::ffff:")[1]
+    if (blackListedIPs.includes(ip)) {
+        res.status(502)
+        return res.send(`Your IP: ${ip} is blacklisted from using our services, have a good day.`)
     } else {
+        res.status(200)
         return res.render("index");
     }
 })
-});
 router.get("/login", (req, res, next) => {
-    return res.render("login")
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    ip = ip.split("::ffff:")[1]
+    if (blackListedIPs.includes(ip)) {
+        res.status(502)
+        return res.send(`Your IP: ${ip} is blacklisted from using our services, have a good day.`)
+    } else {
+        res.status(200)
+        return res.render("login");
+    }
 })
+});
 
 module.exports = router;
