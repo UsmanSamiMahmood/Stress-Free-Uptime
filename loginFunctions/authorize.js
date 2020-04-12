19eb45b7d6a0061e521591ed1f1785c57f6690b7
JonasSchiott <jonas.tysbjerg@gmail.com>
2020-04-12 16:30:50 +0200

    working on generating IDs

--- loginFunctions/authorize.js
+++ loginFunctions/authorize.js
@@ -4,22 +4,23 @@ const { db } = require("../database/handler");
 
 function authorize(email, password) {
     let citiesRef = db.collection('users');
-    let query = citiesRef.where('email', '==', "dawsawd@wadwa.com").get()
-  .then(snapshot => {
-    if (snapshot.empty) {
-      console.log('No matching documents.');
-      return;
-    }  
+    let query = citiesRef.where('email', '==', email).get()
+        .then(snapshot => {
+            if (snapshot.empty) {
+            return false;
+            }  
 
-    snapshot.forEach(doc => {
-        bcrypt.compare(password, doc.data().password, function(err, result) {
-            return true;
+            snapshot.forEach(doc => {
+                bcrypt.compare(password, doc.data().password, function(err, result) {
+                    console.log(typeof result)
+                    return !!result
+                })
+            });
         })
-      console.log(doc.id, '=>', doc.data());
-    });
-  })
-  .catch(err => {
-    console.log('Error getting documents', err);
-  });
+        .catch(err => {
+            console.log('Error getting documents', err);
+        });
 
 }
+
+console.log(authorize("dawsawd@wadwa.com", "2e1r4wq2rwq1f") == true ? "Correct login" : "Incorrect login")
--- loginFunctions/generateClientIDs.js
+++ loginFunctions/generateClientIDs.js
@@ -1,9 +1,26 @@
-const bcrypt = require('bcrypt');
+const { db } = require("../database/handler");
 
-module.exports.hashPassword = function(password, length) {
-    bcrypt.genSalt(length, function(err, salt) {
-        bcrypt.hash(password, length, function(err, hash) {
-            console.log(hash)
-        })
+module.exports.generateUserID = function(length) {
+    let uid = Math.random().toString(36).substring(15);
+    let docRef = db.collection('users').doc(uid);
+    let getDoc = docRef.get()
+    .then(doc => {
+        if (doc.exists) {
+            for (let i = 0; i <= 250; i++) {
+                const nuid = Math.random().toString(36).substring(15);
+                let ref = db.collection("users").doc(nuid);
+                ref.get().then(search => {
+                    if (!search.exists) {
+                        return nuid;
+                    }
+                })
+            }
+        } else {
+            return uid;
+        }
     })
+    .catch(err => {
+        console.error('Error getting document', err);
+    });
+
 }
