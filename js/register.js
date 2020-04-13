636219dead07fafaefa8c27eb104c5c8ef78694e
JonasSchiott <jonas.tysbjerg@gmail.com>
2020-04-13 17:36:05 +0200

    Reuploaded register.ejs & register.js
    Usman, dont you dare delete them again ...

---
+++ js/register.js
@@ -0,0 +1,38 @@
+const database = firebase.database()
+
+window.onload=function(){
+     /* db.collection("data").doc("permissionCheck").get().then(doc => {
+        this.console.log(doc.data())
+    }) */
+
+    const form = document.querySelector("#register-form")
+
+    form.addEventListener('submit', evt => {
+        evt.preventDefault();
+
+        userId = "test"
+        /* var firebaseRef = firebase.database().ref();
+        firebaseRef.child("Text").set("Test") */
+        database.ref("/users/" + userId).set({
+            firstname: "hello"
+        })
+
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+        let passwordConfirm = this.document.getElementById("passwordconfirm").value;
+
+        /* Swal.fire({
+            title: "Success",
+            text: "Registration successful, redirecting...",
+            icon: "success",
+            confirmButtonText: 'Ok'
+        }) */
+        if (password === passwordConfirm) {
+            swal('Success', 'Register successful redirecting...', 'success')
+        } else {
+            swal("Error", "Passwords are not matching!", "error")
+        }
+
+        form.submit()
+    })
+}
