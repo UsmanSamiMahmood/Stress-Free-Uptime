835af5737924ffb7aa745eed776cac65089dd589
SergeantShadoww <47951918+SergeantShadoww@users.noreply.github.com>
2020-04-14 20:17:13 +0100

    Fixed logout functionality.

--- middleware/normal.js
+++ middleware/normal.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const { db } = require("../database/handler");
+const { SESSION_NAME } = require("../app.js")
 
 const redirectToLogin = (req, res, next) => {
     if (!req.session.userID) {
