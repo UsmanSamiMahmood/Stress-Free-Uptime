34da5a369bbe96d6e7c904b3c09b47b8e4ac5ff2
JonasSchiott <jonas.tysbjerg@gmail.com>
2020-05-16 02:22:52 +0200

    make bullshit
    (working on front end, admin.ejs/admin.css

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const app = require("./app");
 const port = process.env.PORT || 80
 const server = http.createServer(app)
 const color = require("colors");
+
 server.listen(port, () => console.log("\nApp listening on port:", `${port}`.bgBlack.brightGreen))
