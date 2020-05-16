98a4180a48b6bbe4f1e95125db46b244b845cd3b
JonasSchiott <jonas.tysbjerg@gmail.com>
2020-05-16 02:14:56 +0200

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,25 +1,3 @@
-// Testing over, will move onto next stage on 21/04/2020.
-
-/* window.onload=function(){
-    $.ajax({
-        method: "POST",
-        url: "/admin",
-        data: {
-            action: 'ban',
-            email: 'usmanmahmood2914@protonmail.com',
-            id: '22zpjw85i',
-        },
-            success: function(body) {
-                var json = JSON.parse(body);
-                swal(json.title, json.message, json.type);
-            },
-
-            error: function(body) {
-                swal("Error Occured", "Failed to connect to back-end server.", "error")
-            }
-    })
-} */
-
 window.onload=function(){
     const form1 = document.querySelector("#ban-form")
 
@@ -66,13 +44,5 @@ window.onload=function(){
 
         })
 
-    })
-
-
-
-    
-
-
-
-    
+    })    
 }
